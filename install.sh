c4134be06b4cff9e3c9d83ca40eff62818605a1c
Ly0kha <falcon1.2010@gmail.com>
2023-01-25 09:03:22 -1000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+chmod +x install.sh
+
+apt-get update
+
+apt-get install python3-nmap python3-termcolor python3-prettytable python3-builtwith python3-requests python3-beautifulsoup4 python3-colorama python3-whois python3-socket
+
+apt-get install python3-pip
+
+pip3 install --upgrade pip
+
+pip3 install nmap termcolor prettytable builtwith requests beautifulsoup4 colorama python-whois
+
+sudo mv samoom.py /usr/local/bin/
+
+sudo chmod +x /usr/local/bin/samoom.py
+
+sudo ln -s /usr/local/bin/samoom.py /usr/local/bin/samoom
