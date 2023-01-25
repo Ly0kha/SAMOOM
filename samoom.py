afaad6b489b9d8714b377fbb1cd20ac1c2e4dbfb
Ly0kha <falcon1.2010@gmail.com>
2023-01-25 09:36:22 -1000

    Update samoom.py

--- samoom.py
+++ samoom.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+import subprocess
+import os
 import nmap
 from termcolor import colored
 from prettytable import PrettyTable
@@ -9,25 +12,33 @@ import whois
 import socket
 
 ##
+os.system('clear')
+os.system('cls')
+
 def print_logo():
     logo = """
-  /$$$$$$   /$$$$$$  /$$      /$$  /$$$$$$   /$$$$$$  /$$      /$$
- /$$__  $$ /$$__  $$| $$$    /$$$ /$$__  $$ /$$__  $$| $$$    /$$$
-| $$  \__/| $$  \ $$| $$$$  /$$$$| $$  \ $$| $$  \ $$| $$$$  /$$$$
-|  $$$$$$ | $$$$$$$$| $$ $$/$$ $$| $$  | $$| $$  | $$| $$ $$/$$ $$
- \____  $$| $$__  $$| $$  $$$| $$| $$  | $$| $$  | $$| $$  $$$| $$
- /$$  \ $$| $$  | $$| $$\  $ | $$| $$  | $$| $$  | $$| $$\  $ | $$
-|  $$$$$$/| $$  | $$| $$ \/  | $$|  $$$$$$/|  $$$$$$/| $$ \/  | $$
- \______/ |__/  |__/|__/     |__/ \______/  \______/ |__/     |__/
-                                                                  
+                  /$$$$$$   /$$$$$$  /$$      /$$  /$$$$$$   /$$$$$$  /$$      /$$
+                 /$$__  $$ /$$__  $$| $$$    /$$$ /$$__  $$ /$$__  $$| $$$    /$$$
+                | $$  \__/| $$  \ $$| $$$$  /$$$$| $$  \ $$| $$  \ $$| $$$$  /$$$$
+                |  $$$$$$ | $$$$$$$$| $$ $$/$$ $$| $$  | $$| $$  | $$| $$ $$/$$ $$
+                 \____  $$| $$__  $$| $$  $$$| $$| $$  | $$| $$  | $$| $$  $$$| $$
+                 /$$  \ $$| $$  | $$| $$\  $ | $$| $$  | $$| $$  | $$| $$\  $ | $$
+                |  $$$$$$/| $$  | $$| $$ \/  | $$|  $$$$$$/|  $$$$$$/| $$ \/  | $$
+                 \______/ |__/  |__/|__/     |__/ \______/  \______/ |__/     |__/
+
+                  SAMOM - Simple Automated Multi-purpose Offensive Manager 
+
+                                    Coded By @Ly0kha                                                
 """
+    
     colored_logo = ""
     for line in logo.split("\n"):
-        colored_logo += colored(line, "green") + "\n"
+        colored_logo += colored(line, "white") + "\n"
     print(colored_logo)
 ## 
+sammomter = "  ╰── Sammom ~# "
 def live_hosts_scan():
-    ip_range = input("Enter the IP range to scan (e.g. 192.168.1.0/24): ")
+    ip_range = input("     Enter the IP range to scan (e.g. 192.168.1.0/24): ")
     nm = nmap.PortScanner()
     nm.scan(ip_range, arguments='-p 21,22,25,80')
     hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
@@ -46,6 +57,18 @@ def live_hosts_scan():
     for host, ports in live_hosts:
         table.add_row([host, ports])
     print(table)
+    while True:
+        choice = input("      Enter '1' to scan again or '2' to go back to the main menu: ")
+        if choice == '1':
+            os.system('clear')
+            live_hosts_scan()
+            break
+        elif choice == '2':
+            os.system('clear')
+            break
+        else:
+            print("Invalid choice. Please enter '1' to scan again or '2' to go back to the main menu.")
+
     #
 
 #whois lookup
@@ -75,6 +98,18 @@ def whois_whois(domain):
         print(table)
     except Exception as e:
         print(e)
+
+    while True:
+        choice = input("Enter '1' to Scan again or '2' to go back to the main menu: ")
+        if choice == '1':
+            os.system('clear')
+            whois.whois(domain)
+            break
+        elif choice == '2':
+            os.system('clear')
+            break
+        else:
+            print("Invalid choice. Please enter '1' to Scan again or '2' to go back to the main menu.")
 #
 def website_tech_scan():
     url = input("Enter the URL to scan: ")
@@ -88,6 +123,26 @@ def website_tech_scan():
             table.add_row([tech, version])
     print(table)
 #
+def dns_enum():
+    domain = input("Enter the domain name to enumerate: ")
+
+    try:
+        subprocess.run(["dnsenum", domain])
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    while True:
+        choice = input("Enter '1' to enumerate again or '2' to go back to the main menu: ")
+        if choice == '1':
+            os.system('clear')
+            dns_enum()
+            break
+        elif choice == '2':
+            os.system('clear')
+            break
+        else:
+            print("Invalid choice. Please enter '1' to enumerate again or '2' to go back to the main menu.")
+            #
 def search_subdomains(main_domain):
     subdomains = []
     search_url = f"https://www.google.com/search?q=site%3A*.{main_domain}"
@@ -108,9 +163,22 @@ def search_subdomains(main_domain):
         table.add_row([f"{subdomain}.{main_domain}"])
     print(table)
     print(colored("Please note that this search result is only based on Google search results. If you are looking for a more in-depth search, it is recommended to use another alternative tool.", "red", attrs=["bold"]))
+#for clear the screen 
+    while True:
+        choice = input("Enter '1' to Search again or '2' to go back to the main menu: ")
+        if choice == '1':
+            os.system('clear')
+            os.system('cls')
+            search_subdomains(main_domain)
+            break
+        elif choice == '2':
+            os.system('clear')
+            os.system('cls')
+            break
+        else:
+            print("Invalid choice. Please enter '1' to Search again or '2' to go back to the main menu.")
 
-
-
+################################################################
 
 #
 def scan_ports():
@@ -134,6 +202,21 @@ def scan_ports():
         print(table)
     else:
         print("No open ports found.")
+#
+    while True:
+        choice = input("Enter '1' to Scan again or '2' to go back to the main menu: ")
+        if choice == '1':
+            os.system('clear')
+            os.system('cls')
+            scan_ports()
+            break
+        elif choice == '2':
+            os.system('clear')
+            os.system('cls')
+            break
+        else:
+            print("Invalid choice. Please enter '1' to Scan again or '2' to go back to the main menu.")
+
 
 def reverse_lookup():
     host = input("Enter the host IP/domain to perform reverse lookup on: ")
@@ -141,23 +224,36 @@ def reverse_lookup():
     try:
         response = requests.get(url)
         print(colored(response.text, 'white'))
+        print("please notice that the reverse lookup using api.hackertarget.com")
     except Exception as e:
         print(colored(f"Error: {e}", 'red'))
+
+    while True:
+        choice = input("Enter '1' to Scan again or '2' to go back to the main menu: ")
+        if choice == '1':
+            os.system('clear')
+            reverse_lookup()
+            break
+        elif choice == '2':
+            os.system('clear')
+            break
+        else:
+            print("Invalid choice. Please enter '1' to Scan again or '2' to go back to the main menu.")        
 # #
 def main():
     while True:
         try:
             print_logo()
-            print(Fore.CYAN + "SAMOM - Simple Automated Multi-purpose Offensive Manager" + Style.RESET_ALL)
-            print("Please choose an option:")
-            print(Fore.GREEN + "1- Port Scanning" + Style.RESET_ALL)
-            print(Fore.GREEN + "2- Live Hosts Scanning" + Style.RESET_ALL)
-            print(Fore.GREEN + "3- Website technologies Scanning" + Style.RESET_ALL)
-            print(Fore.GREEN + "4- Search for Subdomains" + Style.RESET_ALL)
-            print(Fore.GREEN + "5- WHOIS Lookup" + Style.RESET_ALL)
-            print(Fore.GREEN + "6- Reverslookup hosts" + Style.RESET_ALL)
-            print(Fore.RED + "7- Exit" + Style.RESET_ALL)
-            choice = input()
+            print("      Please choose an option:")
+            print(colored("      [1]-- Port Scanning", 'green', attrs=['bold']))
+            print(colored("      [2]-- Live Hosts Scanning", 'green', attrs=['bold']))
+            print(colored("      [3]-- Website technologies Scanning", 'green', attrs=['bold']))
+            print(colored("      [4]-- Search for Subdomains", 'green', attrs=['bold']))
+            print(colored("      [5]-- WHOIS Lookup", 'green', attrs=['bold']))
+            print(colored("      [6]-- Reverslookup Hosts", 'green', attrs=['bold']))
+            print(colored("      [7]-- DNS Enumeration", 'green', attrs=['bold']))
+            print(colored("      [8]-- EXIT", 'red', attrs=['bold']))
+            choice = input(sammomter)
             if choice == "1":
                 scan_ports()
             elif choice == "2":
@@ -173,6 +269,8 @@ def main():
             elif choice == "6":
                 reverse_lookup()
             elif choice == "7":
+                dns_enum()
+            elif choice == "8":
                 break
             else:
                 print(Fore.RED + "Invalid choice. Please try again." + Style.RESET_ALL)
