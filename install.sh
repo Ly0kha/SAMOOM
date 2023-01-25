9f467aa509b4a5d25336ab8c186d771140cfbe18
Ly0kha <falcon1.2010@gmail.com>
2023-01-25 09:49:46 -1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-chmod +x install.sh
 
 apt-get update
 
-apt-get install python3-nmap python3-termcolor python3-prettytable python3-builtwith python3-requests python3-beautifulsoup4 python3-colorama python3-whois python3-socket python3-json
+apt-get install python3-nmap python3-termcolor python3-prettytable python3-builtwith python3-requests python3-beautifulsoup4 python3-colorama python3-whois python3-socket
 
 apt-get install python3-pip
 
 pip3 install --upgrade pip
 
-pip3 install nmap termcolor prettytable builtwith requests beautifulsoup4 colorama python-whois json
+pip3 install nmap termcolor prettytable builtwith requests beautifulsoup4 colorama python-whois
 
 sudo mv samoom.py /usr/local/bin/
 
 sudo chmod +x /usr/local/bin/samoom.py
 
-sudo ln -s /usr/local/bin/samoom.py /usr/local/bin/samoom
+sudo ln -sf /usr/local/bin/samoom.py /usr/local/bin/samoom
